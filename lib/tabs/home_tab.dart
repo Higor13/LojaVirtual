4abf5c6b8f2c8b770fc2822aa9b3765e0cd3b2b0
HigorPereira <higor.pereira@mobrj.com.br>
2022-11-30 16:51:27 -0300

    loading images in home tab

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomeTab extends StatelessWidget {
   const HomeTab({super.key});
@@ -32,6 +35,43 @@ class HomeTab extends StatelessWidget {
                 title: const Text('Novidades'),
                 centerTitle: true,
               ),
+            ),
+            FutureBuilder<QuerySnapshot>(
+              builder: ((context, snapshot) {
+                if (!snapshot.hasData) {
+                  return SliverToBoxAdapter(
+                    child: Container(
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                  );
+                } else {
+                  return SliverStaggeredGrid.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 1.0,
+                    crossAxisSpacing: 1.0,
+                    staggeredTiles: snapshot.data!.docs.map(
+                      (doc) {
+                        return StaggeredTile.count(2, 2);
+                      },
+                    ).toList(),
+                    children: snapshot.data!.docs.map((doc) {
+                      return FadeInImage.memoryNetwork(
+                        placeholder: kTransparentImage,
+                        image: doc['image'],
+                        fit: BoxFit.cover,
+                      );
+                    }).toList(),
+                  );
+                }
+              }),
+              future: FirebaseFirestore.instance
+                  .collection('home')
+                  .orderBy('pos')
+                  .get(),
             ),
           ],
         ),
