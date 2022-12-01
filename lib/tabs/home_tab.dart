643cf706d69d6f13b5d4c0bef983cc682365f9fe
HigorPereira <higor.pereira@mobrj.com.br>
2022-12-01 08:51:08 -0300

    fixing image y size in home tab

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -55,7 +55,7 @@ class HomeTab extends StatelessWidget {
                     crossAxisSpacing: 1.0,
                     staggeredTiles: snapshot.data!.docs.map(
                       (doc) {
-                        return StaggeredTile.count(doc['x'], 1);
+                        return StaggeredTile.count(doc['x'], doc['y'] + .0);
                       },
                     ).toList(),
                     children: snapshot.data!.docs.map((doc) {
