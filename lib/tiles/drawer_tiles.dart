bd31d5b33dcd2b4029cfebd997b0eaa37e73ae2b
HigorPereira <higor.pereira@mobrj.com.br>
2022-12-01 14:53:32 -0300

    Selecting pages from drawer

--- lib/tiles/drawer_tiles.dart
+++ lib/tiles/drawer_tiles.dart
@@ -3,15 +3,20 @@ import 'package:flutter/material.dart';
 class DrawerTiles extends StatelessWidget {
   final IconData icon;
   final String text;
+  final PageController pageController;
+  final int page;
 
-  DrawerTiles(this.icon, this.text);
+  DrawerTiles(this.icon, this.text, this.pageController, this.page);
 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.transparent,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          Navigator.of(context).pop(); // Closes the drawer
+          pageController.jumpToPage(page);
+        },
         child: Container(
           height: 60.0,
           child: Row(
