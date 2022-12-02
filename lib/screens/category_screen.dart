9ba0e7934f6cc0f1162d74506a4841fef09416a2
HigorPereira <higor.pereira@mobrj.com.br>
2022-12-02 08:59:46 -0300

    Grid for products and product_tile created

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,5 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:lojavirtual/data/product_data.dart';
+
+import '../tiles/product_tile.dart';
 
 class CategoryScreen extends StatelessWidget {
   // const CategoryScreen({ super.key });
@@ -16,7 +19,7 @@ class CategoryScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text(snapshot['title']),
           centerTitle: true,
-          bottom: TabBar(tabs: [
+          bottom: const TabBar(tabs: [
             Tab(
               icon: Icon(Icons.grid_on),
             ),
@@ -25,16 +28,45 @@ class CategoryScreen extends StatelessWidget {
             ),
           ]),
         ),
-        body: TabBarView(
-          physics: NeverScrollableScrollPhysics(),
-          children: [
-            Container(
-              color: Colors.red,
-            ),
-            Container(
-              color: Colors.green,
-            ),
-          ],
+        body: FutureBuilder<QuerySnapshot>(
+          future: FirebaseFirestore.instance
+              .collection('products')
+              .doc(snapshot.id)
+              .collection('items')
+              .get(),
+          builder: ((context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              return TabBarView(
+                physics: const NeverScrollableScrollPhysics(),
+                children: [
+                  GridView.builder(
+                    padding: const EdgeInsets.all(4.0),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 4.0,
+                      crossAxisSpacing: 4.0,
+                      childAspectRatio: 0.65,
+                    ),
+                    itemCount: snapshot.data?.docs.length,
+                    itemBuilder: ((context, index) {
+                      return ProductTile(
+                        'grid',
+                        ProductData.fromDocument(snapshot.data!.docs[index]),
+                      );
+                    }),
+                  ),
+                  Container(
+                    color: Colors.green,
+                  ),
+                ],
+              );
+            }
+          }),
         ),
       ),
     );
---
+++ lib/tiles/product_tile.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:lojavirtual/data/product_data.dart';
+
+class ProductTile extends StatelessWidget {
+  // const ProductTile({super.key});
+
+  final String type;
+  final ProductData data;
+
+  ProductTile(this.type, this.data);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
