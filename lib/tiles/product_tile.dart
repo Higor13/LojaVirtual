a849455ec723ce5456c13bd58b0b7fed5403ca6e
HigorPereira <higor.pereira@mobrj.com.br>
2022-12-02 10:28:43 -0300

    List products ready

--- lib/tiles/product_tile.dart
+++ lib/tiles/product_tile.dart
@@ -49,7 +49,41 @@ class ProductTile extends StatelessWidget {
                   ),
                 ],
               )
-            : Row(),
+            : Row(
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: Image.network(
+                      product.images[0],
+                      fit: BoxFit.cover,
+                      height: 250.0,
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            product.title,
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            'R\$ ${product.price.toStringAsFixed(2)}',
+                            style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 17.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
       ),
     );
   }
