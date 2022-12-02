78744b0aed3a28d8d38ae2bf9f3c692af1e8f3b6
HigorPereira <higor.pereira@mobrj.com.br>
2022-12-02 10:22:08 -0300

    Products grid ready

--- lib/tiles/product_tile.dart
+++ lib/tiles/product_tile.dart
@@ -5,12 +5,52 @@ class ProductTile extends StatelessWidget {
   // const ProductTile({super.key});
 
   final String type;
-  final ProductData data;
+  final ProductData product;
 
-  ProductTile(this.type, this.data);
+  ProductTile(this.type, this.product);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return InkWell(
+      child: Card(
+        child: type == 'grid'
+            ? Column(
+                crossAxisAlignment:
+                    CrossAxisAlignment.stretch, // Imagem esticada
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  AspectRatio(
+                    aspectRatio: 0.8,
+                    child: Image.network(
+                      product.images[0],
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            product.title,
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            'R\$ ${product.price.toStringAsFixed(2)}',
+                            style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 17.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            : Row(),
+      ),
+    );
   }
 }
