4b189ddc88125583ef0ee0b62d97d177278a7252
HigorPereira <higor.pereira@mobrj.com.br>
2022-12-02 14:08:44 -0300

    validating login page fields

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  LoginScreen({super.key});
+
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +23,7 @@ class LoginScreen extends StatelessWidget {
         ],
       ),
       body: Form(
+        key: _formKey,
         child: ListView(
           padding: const EdgeInsets.all(16.0),
           children: [
@@ -29,12 +32,22 @@ class LoginScreen extends StatelessWidget {
                 hintText: 'Email',
               ),
               keyboardType: TextInputType.emailAddress,
+              validator: ((text) {
+                if (text!.isEmpty || !text.contains('@')) {
+                  return 'Email inválido';
+                }
+              }),
             ),
             TextFormField(
               decoration: const InputDecoration(
                 hintText: 'Senha',
               ),
               obscureText: true,
+              validator: ((text) {
+                if (text!.isEmpty || text.length < 6) {
+                  return 'Senha inválida';
+                }
+              }),
             ),
             Align(
               alignment: Alignment.centerRight,
@@ -53,7 +66,9 @@ class LoginScreen extends StatelessWidget {
             SizedBox(
               height: 44.0,
               child: ElevatedButton(
-                onPressed: (() {}),
+                onPressed: (() {
+                  if (_formKey.currentState!.validate()) {}
+                }),
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Theme.of(context).primaryColor,
                 ),
