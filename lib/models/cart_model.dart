efca1be6e6f59149c4bd1a0cc1bd0bd91bce67a8
HigorPereira <higor.pereira@mobrj.com.br>
2022-12-06 08:35:26 -0300

    add and remove CartItem methods added

---
+++ lib/models/cart_model.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:lojavirtual/data/cart_product.dart';
+import 'package:lojavirtual/models/user_model.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class CartModel extends Model {
+  UserModel user;
+  List<CartProduct> products = [];
+
+  CartModel(this.user);
+
+  void addCartItem(CartProduct cartProduct) {
+    products.add(cartProduct); // Adding to list
+
+    // Adding to firebase
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc(user.firebaseUser?.user?.uid)
+        .collection('cart')
+        .add(cartProduct.toMap())
+        .then((doc) {
+      cartProduct.cid = doc.id;
+    });
+
+    notifyListeners();
+  }
+
+  void removeCartItem(CartProduct cartProduct) {
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc(user.firebaseUser?.user?.uid)
+        .collection('cart')
+        .doc(cartProduct.cid)
+        .delete();
+
+    products.remove(cartProduct);
+    notifyListeners();
+  }
+}
